3e31e5c7fa63c54ff374ed1333a77719a40f3299
Roy Daniel <56827243+fxcircus@users.noreply.github.com>
2024-04-16 17:51:38 -0400

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,7 @@
-// app.component.ts
-
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { FronteggAppService, FronteggAuthService, ContextHolder } from '@frontegg/angular';
-import { ITeamUserRole } from '@frontegg/rest-api';
+import { ITeamUserRole, setTabTenantInSessionStorage } from '@frontegg/rest-api';
 
 @Component({
   selector: 'app-root',
@@ -14,27 +12,37 @@ export class AppComponent implements OnInit, OnDestroy {
   isLoading = true;
   loadingSubscription: Subscription;
   user?: any;
-  selectedTenantId: string = ''; // Initialize to an empty string
+  selectedTenantId: string = '';
+  tenantState?: any;
 
   constructor(
     private fronteggAuthService: FronteggAuthService,
     private fronteggAppService: FronteggAppService
   ) {
-    this.loadingSubscription = fronteggAppService.isLoading$.subscribe((isLoading) => (this.isLoading = isLoading));
+    this.loadingSubscription = this.fronteggAppService.isLoading$.subscribe(
+      (isLoading) => (this.isLoading = isLoading)
+    );
   }
 
   ngOnInit(): void {
-    this.fronteggAuthService?.teamState$.subscribe((teamState) => {
+    this.fronteggAuthService.teamState$.subscribe((teamState) => {
       const newRoles: ITeamUserRole[] = teamState.roles.filter((role: ITeamUserRole) => role.name !== 'New');
       this.fronteggAuthService.setTeamState({ roles: newRoles });
     });
 
-    this.fronteggAuthService?.user$.subscribe((user) => {
+    this.fronteggAuthService.user$.subscribe((user) => {
       this.user = user;
+    });
+
+    this.fronteggAuthService.loadTenants();
+    this.fronteggAuthService.tenantsState$.subscribe((tenants) => {
+      this.tenantState = tenants;
+      console.log('Tenant State Loaded:', this.tenantState);  // Log tenant state after loading
     });
   }
 
   switchTenant(): void {
+    console.log('Switching to Tenant ID:', this.selectedTenantId);  // Log the tenant ID on switch
     this.fronteggAuthService.switchTenant({ tenantId: this.selectedTenantId });
   }
 
@@ -43,15 +51,16 @@ export class AppComponent implements OnInit, OnDestroy {
   }
 
   loginToTenantX(): void {
-    this.loginWithRedirectToTenant('36ad8775-ea37-4b60-8032-8738af07be87');
+    this.loginWithRedirectToTenant('your-tenant-id');
   }
 
   private loginWithRedirectToTenant(tenantId: string): void {
     console.log(`Logging in to Tenant ${tenantId}`);
-    this.fronteggAuthService.loginWithRedirect({ tenantId: tenantId, prompt: 'login'});
+    this.fronteggAuthService.loginWithRedirect({ tenantId, prompt: 'login' });
   }
 
   logOut(): void {
+    setTabTenantInSessionStorage("");
     const baseUrl = ContextHolder.getContext().baseUrl;
     window.location.href = `${baseUrl}/oauth/logout?post_logout_redirect_uri=${window.location}`;
   }
@@ -61,6 +70,6 @@ export class AppComponent implements OnInit, OnDestroy {
   }
 
   refreshToken(): void {
-    this.fronteggAuthService.requestAuthorize()
+    this.fronteggAuthService.requestAuthorize();
   }
 }
